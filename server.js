90c7a8cbd2243d06a84ba9b0bb0ffd37b58359d5
AhmadJRDeveloper <ahmadsaad200235@gmail.com>
2024-03-11 21:23:42 +0200

    testing

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "lak allah maao" })
+})
+
+app.listen(4000, () => {
+    console.log('server listen on port 4000')
+})
